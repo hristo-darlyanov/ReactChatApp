ff21687d9c4469dc3ed19a10ff61c9cee4ebc697
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-08-06 19:37:39 +0300

    Created style for RegisterView.js and HomeView.js

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ChatView from './view/ChatView';
 import LoginView from './view/LoginView';
+import RegisterView from './view/RegisterView';
+import HomeView from './view/HomeView'
 
 const Stack = createNativeStackNavigator()
+const AuthenticatedUserContext = createContext()
 
 function ChatStack() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={LoginView}/>
+      <Stack.Screen name="Register" component={RegisterView}/>
       {/* <Stack.Screen name="Chat" component={ChatView}/> */}
     </Stack.Navigator>
   );
